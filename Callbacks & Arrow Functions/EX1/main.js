7c8482bcc680728376dcc32b3fbb050399b989d5
shiran987 <shiransh987@gmail.com>
2020-11-03 18:20:48 +0200

    ex4+ex5

---
+++ Callbacks & Arrow Functions/EX1/main.js
@@ -0,0 +1,35 @@
+// //ex1
+// const pushPull = function (callback ){
+// const push = function () {
+//     setTimeout(function () {
+//     console.log("pushing it!")
+//     callback() 
+// }, 3000)
+//   }
+  
+//   const pull = function () {
+//     console.log("pulling it!")
+//   }
+  
+//   pushPull(push) //should print "pushing it!"
+//   pushPull(pull) //should print "pulling it!"
+  
+// }
+
+
+  
+
+// ex4
+const add = (x, y ,z) => x + y +z
+const sum = add(1, 2, 3) 
+console.log(sum)
+
+// ex5
+const capitalize= (str) => str.toUpperCase();
+
+ capitalization1=capitalize("bOb")
+ capitalization2=capitalize("TAYLOR")
+ capitalization3=capitalize("feliSHIA")
+console.log(capitalization1)
+console.log(capitalization2)
+console.log(capitalization3)
