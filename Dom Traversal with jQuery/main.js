96a6a5c730636f51d08bcbbacee0566f7ca9a8c8
shiran987 <shiransh987@gmail.com>
2020-11-05 17:41:38 +0200

    dom travesal with jquery

---
+++ Dom Traversal with jQuery/main.js
@@ -0,0 +1,23 @@
+//1+2+3
+let array=[]
+  $('.generator').on('click', function() {
+     let dataid=$(this).closest(".computer").data().id 
+     console.log($(this).closest(".computer").find(".processor").attr('id'))
+     console.log($(this).closest(".computer").find(".BUS").text())
+  
+    console.log(dataid);
+     newobject={["cmp_id"]: dataid}
+    array.push(newobject)
+  console.log(array)
+  })
+
+
+
+//4+5+6
+  
+$('.validator').on('click', function() {
+    console.log($(this).closest(".computer").find(".generator").text())
+    console.log($(this).closest(".computer").find(".MB").text())
+    console.log($(this).closest(".computer").find(".QR").text())
+  
+  })
